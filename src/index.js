14d097333d5002fa2c2508ad9b48c45bb98bedb7
Zlatko Fedor <zfedor@gmail.com>
2015-12-25 23:01:55 +0100

    Release 0.1.23

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import RandomText from './layers/RandomText';
 import CoverType from './constants/CoverType';
 import LayerType from './constants/LayerType';
 import ResizeType from './constants/ResizeType';
+import TextAlign from './constants/TextAlign';
+import TextBaseline from './constants/TextBaseline';
 
 import parseLayers from './utils/parseLayers';
 
-export { Layer, Text, Photo, Img, RandomText, CoverType, LayerType, ResizeType, parseLayers };
+export { Layer, Text, Photo, Img, RandomText, CoverType, LayerType,
+  ResizeType, parseLayers, TextAlign, TextBaseline };
 export default Paper;
